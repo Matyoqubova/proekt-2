84575db34356c51608f2c1b6d9fc98c1c0f3e48e
Matyoqubova <matyoqubovagulbahor1@gmail.com>
2026-02-20 09:38:31 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,85 @@
-let todoInputEl = document.querySelector(".todo-input");
-let btnAddEl = document.querySelector(".btn-add");
-let ulDisplayEl = document.querySelector(".ul-display");
+// alert("Salom JS")
+const date = new Date();
+const doc = document.getElementById("soat");
+const docum = document.getElementById("sana");
 
-let htmlText = "<li>first todo</li>";
+let sana = date.getDate();
+let haftaIndex = date.getDay();
+let haftaKuni = [
+  "yakshanba",
+  "Dushanba",
+  "Seshanba",
+  "Chorshanba",
+  "Payshanba",
+  "Juma",
+  "Shanba",
+];
+let oy = [
+  "Yanvar",
+  "Fev",
+  "Mart",
+  "Apr",
+  "May",
+  "Iyn",
+  "Iyl",
+  "Avg",
+  "Sen",
+  "Noy",
+  "Dek",
+];
+let yil = date.getFullYear();
 
-btnAddEl.addEventListener("click", () => {
-  const inputValue = todoInputEl.value;
-  if (inputValue.trim().length < 1) {
-    alert("Iltimos oldin todo kiriting!!!");
-    return;
-  }
-  htmlText += `<li> ${inputValue} </li>`;
-  ulDisplayEl.innerHTML = htmlText;
-  console.log(htmlText);
-  todoInputEl.value = "";
-});
+let soat = date.getHours();
+let millisekund = date.getMilliseconds();
+let minut = date.getMinutes();
+let sekund = date.getSeconds();
+
+let text = `${sana}. ${oy[0]}. ${yil}.${haftaKuni[haftaIndex]}`;
+let texttt = `${soat}:${minut}:${sekund}:${millisekund}`;
+console.log(text);
+console.log(texttt);
+
+let inner = (doc.innerHTML = texttt);
+let inner2 = (docum.innerHTML = text);
+
+
+let bodyElement =document.body;
+bodyElement.style.background = "linear-gradient(155deg, gold,blue,green)";
+
+
+function onBottonClik() {
+  console.log("on Clik");
+  
+}
+function onbuttondown() {
+  console.log("on down");
+  
+}
+function onButtonup() {
+  console.log("on up");
+  
+}
+function onButtonOut() {
+  
+}
+function onButtonOver() {
+  console.log("on over");
+  
+}
+
+function red() {
+  let doc = document.getElementById("box")
+  doc.style.backgroundColor = "red"
+}
+function yellow() {
+  let doc = document.getElementById("box")
+  doc.style.backgroundColor = "yellow"
+}
+function black() {
+  let doc = document.getElementById("box")
+  doc.style.backgroundColor = "black"
+}
+function blue() {
+  let doc = document.getElementById("box")
+  doc.style.backgroundColor = "blue"
+}
